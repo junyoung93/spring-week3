c308999ae3c4abada21aea12037eb94b594c48b3
PARKJUNYOUNG\USER <yyijs0309@gmail.com>
2023-06-30 19:44:50 +0900

    week3 init commit

--- src/main/java/com/sparta/springauth/SpringAuthApplication.java
+++ src/main/java/com/sparta/springauth/SpringAuthApplication.java
@@ -2,8 +2,9 @@ package com.sparta.springauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
 public class SpringAuthApplication {
 
     public static void main(String[] args) {
